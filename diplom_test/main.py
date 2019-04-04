af2416d9e8763cf92025e3cf94b057e8f3ca1afa
CrackedStone <iljankovy@gmail.com>
2019-04-04 11:39:35 +0300

    changed the using of the permutation entropy tried to have a look at the different patterns probability in the image and chose two of them were the most different in patho- and normal groups

--- diplom_test/main.py
+++ diplom_test/main.py
@@ -8,7 +8,7 @@ import gradients as grds
 from PIL import Image
 
 #download folder BMP
-def getBMP(fullDir,lastDir):
+def getBMP(fullDir, lastDir):
     # считаем количество изображений
     fileNumber = len(next(os.walk(fullDir))[2])
     print(fileNumber, "pictures were found")
@@ -22,7 +22,9 @@ def getBMP(fullDir,lastDir):
 def rgb2gray(rgb):
     #скалярное произведение начальных цветов с определенными теоретическими коэффициентами по системе YUV
     return np.dot(rgb[...,:3], [0.333, 0.333, 0.333]).round(3).astype(int)
-def averageRGB(img,windW,windH):
+
+
+def averageRGB(img, windW, windH):
     #укрупним области
     w = len(img[0])
     h = len(img)
@@ -45,7 +47,9 @@ def averageRGB(img,windW,windH):
                     ed[i+a][j+b][1] = G
                     ed[i+a][j+b][2] = B
     return ed
-def averageGray(img,windW,windH):
+
+
+def averageGray(img, windW, windH):
     #укрупним области
     w = len(img[0])
     h = len(img)
@@ -76,80 +80,119 @@ def normalizeArray2D(table):
             rez[i][j] = table[i][j]/sum
     return rez
 
-'''
-#===========рабочее считывание изображений===========
-print("... for pathology of a kidney parenchyma for our ultrasonography:")
-pathoFull = "C:/Users/admin/PycharmProject/diplom_test/pathology"
+
+# reading of our images
+print("Donwloading images for a pathological and a normal state of a kidney parenchyma for our ultrasonography:")
+pathoFull = "C:/Users/admin/PycharmProject/algorithm-detects-liver-pathology/diplom_test/pathology"
 pathoLast = "pathology/"
 pathoBMP = getBMP(pathoFull,pathoLast)
 for i in range (len(pathoBMP)):
     pathoBMP[i] = rgb2gray(pathoBMP[i])
-normaFull = "C:/Users/admin/PycharmProject/diplom_test/norma"
+normaFull = "C:/Users/admin/PycharmProject/algorithm-detects-liver-pathology/diplom_test/norma"
 normaLast = "norma/"
 normaBMP = getBMP(normaFull,normaLast)
 for i in range (len(normaBMP)):
     normaBMP[i] = rgb2gray(normaBMP[i])
-'''
-'''
-#========рабочие рассчеты PERMUTATION ENTROPY========
+
+# PERMUTATION ENTROPY calculations and processing
+patternNumber = 5
+patternAXIS1 = 2
+patternAXIS2 = 3
+
 print('\nPathological specimens')
-pathoPEnzed, pathoPEmid = calculateDiffPEvalues(pathoBMP)
-plt.plot(pathoPEnzed,pathoPEmid,'ro',label='pathological samples')
+pathoPEpatterns = np.zeros((1, patternNumber))
+for i in range(len(pathoBMP)):
+    tmp = pe.pe(pathoBMP[i])
+    tmp.PErawAnalysis()
+    for j in range(patternNumber):
+        pathoPEpatterns[0][j] += tmp.rez[0][j]
+        plt.plot(tmp.rez[0][patternAXIS1],
+                 tmp.rez[0][patternAXIS2],
+                 'ro')
+for k in range(patternNumber):
+    pathoPEpatterns[0][k] /= len(pathoBMP)
+plt.plot(pathoPEpatterns[0][patternAXIS1],
+         pathoPEpatterns[0][patternAXIS2],
+         'rs', label='pathological AVERAGE')
+print(pathoPEpatterns)
+
 print('Normal specimens')
-normaPEnzed, normaPEmid = calculateDiffPEvalues(normaBMP)
-plt.plot(normaPEnzed,normaPEmid,'g^',label='normal samples')
-plt.xlabel("the normalized permutation entropy of the PE")
-plt.ylabel("the mean of the normalized permutation entropy")
+normaPEpatterns = np.zeros((1, patternNumber))
+for i in range(len(normaBMP)):
+    tmp = pe.pe(normaBMP[i])
+    tmp.PErawAnalysis()
+    for j in range(patternNumber):
+        normaPEpatterns[0][j] += tmp.rez[0][j]
+        plt.plot(tmp.rez[0][patternAXIS1],
+                 tmp.rez[0][patternAXIS2],
+                 'g^')
+for k in range(patternNumber):
+    normaPEpatterns[0][k] /= len(normaBMP)
+plt.plot(pathoPEpatterns[0][patternAXIS1],
+         pathoPEpatterns[0][patternAXIS2],
+         'gs', label='normal AVERAGE')
+print(normaPEpatterns)
+
+perc = np.zeros((1, patternNumber))
+for i in range(patternNumber):
+    dif = abs(normaPEpatterns[0][i] - pathoPEpatterns[0][i])
+    perc[0][i] = (dif / max(normaPEpatterns[0][i], pathoPEpatterns[0][i])) * 100
+print('Normalized difference in percents:')
+print(perc)
+
+plt.xlabel("ЗРОСТАННЯ, %")
+plt.ylabel("СПАДАННЯ, %")
 plt.legend()
-##============================================
+
+# считывание 1го изображение для тестов
 '''
-#считывание 1го изображение для тестов
-original = mpimg.imread("norma/5.bmp")
-# original = original[0:20,0:20,:]
+original = mpimg.imread("pathology/5.bmp")
+original = original[0:20,0:20,:]
 #plt.subplot(211)
 #plt.imshow(original)
 #plt.title("Оригінальне зображення")
 plt.imsave("tmp/tmp1.png", original)
 rawGray = rgb2gray(original)
 gray = np.array(rawGray)
-#calculation = glcm.GLCM(gray).glcm_complex_duplex()
+calculation = glcm.GLCM(gray).glcm_complex_duplex()
 grCoMap = plt.get_cmap('gray')
-# GLCM
+# GLCM results' saving
+#plt.imshow(calculation,cmap=grCoMap)
+#plt.imsave("tmp/tmp2.png",gray, cmap=grCoMap)
+
 
 fig = plt.figure()
 ax = fig.add_subplot(311)
-ax.imshow(gray, cmap=grCoMap)
-# ax.title("Original")
-# plt.imshow(calculation,cmap=grCoMap)
-# plt.imsave("tmp/tmp2.png",gray, cmap=grCoMap)
+plt.imshow(gray, cmap=grCoMap)
+plt.title("Original")
+'''
 
+# this code computes and plots gradients of the image
+'''
 dif = grds.gradient(gray)
 plt.subplot(323)
 dif.computeHorizontal()
-plt.imshow(dif.getHorizontal())
+plt.imshow(dif.getHorizontal(),cmap="inferno")
 plt.title("Horizontal")
-
 plt.subplot(324)
 dif.computeVertical()
-plt.imshow(dif.getVertical())
+plt.imshow(dif.getVertical(),cmap="inferno")
 plt.title("Vertical")
-
 plt.subplot(325)
 dif.computeDiagonal135()
-plt.imshow(dif.getDiagonal135())
+plt.imshow(dif.getDiagonal135(),cmap="inferno")
 plt.title("Diagonal135")
-
 plt.subplot(326)
 dif.computeDiagonal45()
-plt.imshow(dif.getDiagonal45())
+plt.imshow(dif.getDiagonal45(),cmap="inferno")
 plt.title("Diagonal45")
 fig.tight_layout()
-
-fig.savefig("tmp/spec.pdf")
-
-
+## save
+#fig.savefig("tmp/p5.pdf")
 '''
-#===========спроба фарбувати ПІЛ===========
+
+# attempt to plot the red-blue mask on the gray image
+'''
 #img = Image.fromarray(grayAsRGB.astype(np.uint8))
 img = Image.open("tmp/tmp2.png")
 plt.subplot(312)
@@ -198,6 +241,7 @@ plt.imshow(im2arr)
 plt.title("Зображення, покрите маскою на основі GLCM")
 '''
 
+# labels for GLCM
 '''
 plt.title("Color-Color symmetrical GLCM")
 plt.xlabel("color1")
