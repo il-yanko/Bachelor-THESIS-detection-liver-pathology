190718c2451fb44516500317aa992491ce2a921d
ikachko <ilyakachko@gmail.com>
2019-04-09 23:11:40 +0300

    code for loading all glcm calculations into separate datasets

--- diplom_test/src/probabilities.py
+++ diplom_test/src/probabilities.py
@@ -5,7 +5,7 @@ from img_reader import IMGReader
 import numpy as np
 import pandas as pd
 
-PROJECT_PWD = "/Users/ilyakachko/Diploma/diplom_test"
+PROJECT_PWD = "/Users/ilyakachko/Diploma/algorithm-detects-liver-pathology/diplom_test"
 
 # PROJECT_PWD = "/Users/ikachko/Diploma/algorithm-detects-liver-pathology/diplom_test"
 
@@ -16,8 +16,9 @@ norma_imgs_names, norma_imgs = IMGReader.read_directory(NORMA_DIR)
 pathology_img_names, pathology_imgs = IMGReader.read_directory(PATHOLOGY_DIR)
 
 def count_probabilities_arr(array):
-    print(array)
     vector = np.asarray(array.flatten(), dtype=np.float64)
+    # print(vector)
+
     sum_of_all = vector.sum()
 
     prob_matrix = np.zeros(array.shape)
@@ -124,13 +125,94 @@ dict_glcm_0 = {
     'pathology': []
 }
 
-for name, img in zip(pathology_img_names, pathology_imgs):
-    dict_glcm_0['name'].append(name)
-    glcm = GLCM(img)
-    matrix = glcm.glcm_0()
+dict_glcm_45 = {
+    'name': [],
+    'energy': [],
+    'contrast': [],
+    'homogenity': [],
+    'correlation': [],
+    'variance': [],
+    'entropy': [],
+    'pathology': []
+}
 
-    probs = count_probabilities_arr(matrix)
+
+dict_glcm_90 = {
+    'name': [],
+    'energy': [],
+    'contrast': [],
+    'homogenity': [],
+    'correlation': [],
+    'variance': [],
+    'entropy': [],
+    'pathology': []
+}
+
+
+dict_glcm_135 = {
+    'name': [],
+    'energy': [],
+    'contrast': [],
+    'homogenity': [],
+    'correlation': [],
+    'variance': [],
+    'entropy': [],
+    'pathology': []
+}
+
+
+dict_glcm_complex = {
+    'name': [],
+    'energy': [],
+    'contrast': [],
+    'homogenity': [],
+    'correlation': [],
+    'variance': [],
+    'entropy': [],
+    'pathology': []
+}
+
+
+dict_glcm_gen_duplex = {
+    'name': [],
+    'energy': [],
+    'contrast': [],
+    'homogenity': [],
+    'correlation': [],
+    'variance': [],
+    'entropy': [],
+    'pathology': []
+}
+
+
+dict_glcm_complex_duplex = {
+    'name': [],
+    'energy': [],
+    'contrast': [],
+    'homogenity': [],
+    'correlation': [],
+    'variance': [],
+    'entropy': [],
+    'pathology': []
+}
+# glcm = GLCM(pathology_imgs[0])
+# g = glcm.glcm_0()
+# p = count_probabilities_arr(g)
+# gleq = GLCMEquations(p)
+# print("Energy: ", gleq.energy())
+# print("Contrast: ", gleq.contrast())
+# print("Homogenity: ", gleq.homogenity())
+# print("Correlation: ", gleq.correlation())
+# print("Variance:", gleq.variance())
+# print("Entropy: ", gleq.entropy())
+
+for name, img in zip(pathology_img_names, pathology_imgs):
+    glcm = GLCM(img)
+    matrix_gl_0 = glcm.glcm_0()
+
+    probs = count_probabilities_arr(matrix_gl_0)
     glcm_eq = GLCMEquations(probs)
+    dict_glcm_0['name'].append('patho_' + name)
     dict_glcm_0['energy'].append(glcm_eq.energy())
     dict_glcm_0['contrast'].append(glcm_eq.contrast())
     dict_glcm_0['homogenity'].append(glcm_eq.homogenity())
@@ -139,13 +221,88 @@ for name, img in zip(pathology_img_names, pathology_imgs):
     dict_glcm_0['entropy'].append(glcm_eq.entropy())
     dict_glcm_0['pathology'].append(1)
 
-for name, img in zip(norma_imgs_names, norma_imgs):
-    dict_glcm_0['name'].append(name)
-    glcm = GLCM(img)
-    matrix = glcm.glcm_0()
-
-    probs = count_probabilities_arr(matrix)
+    matrix_gl_45 = glcm.glcm_45()
+    probs = count_probabilities_arr(matrix_gl_45)
     glcm_eq = GLCMEquations(probs)
+    dict_glcm_45['name'].append('patho_' + name)
+    dict_glcm_45['energy'].append(glcm_eq.energy())
+    dict_glcm_45['contrast'].append(glcm_eq.contrast())
+    dict_glcm_45['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_45['correlation'].append(glcm_eq.correlation())
+    dict_glcm_45['variance'].append(glcm_eq.variance())
+    dict_glcm_45['entropy'].append(glcm_eq.entropy())
+    dict_glcm_45['pathology'].append(1)
+
+    matrix_gl_90 = glcm.glcm_90()
+    probs = count_probabilities_arr(matrix_gl_90)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_90['name'].append('patho_' + name)
+    dict_glcm_90['energy'].append(glcm_eq.energy())
+    dict_glcm_90['contrast'].append(glcm_eq.contrast())
+    dict_glcm_90['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_90['correlation'].append(glcm_eq.correlation())
+    dict_glcm_90['variance'].append(glcm_eq.variance())
+    dict_glcm_90['entropy'].append(glcm_eq.entropy())
+    dict_glcm_90['pathology'].append(1)
+
+    matrix_gl_135 = glcm.glcm_135()
+    probs = count_probabilities_arr(matrix_gl_135)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_135['name'].append('patho_' + name)
+    dict_glcm_135['energy'].append(glcm_eq.energy())
+    dict_glcm_135['contrast'].append(glcm_eq.contrast())
+    dict_glcm_135['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_135['correlation'].append(glcm_eq.correlation())
+    dict_glcm_135['variance'].append(glcm_eq.variance())
+    dict_glcm_135['entropy'].append(glcm_eq.entropy())
+    dict_glcm_135['pathology'].append(1)
+
+    matrix_gl_complex = glcm.glcm_complex()
+    probs = count_probabilities_arr(matrix_gl_complex)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_complex['name'].append('patho_' + name)
+    dict_glcm_complex['energy'].append(glcm_eq.energy())
+    dict_glcm_complex['contrast'].append(glcm_eq.contrast())
+    dict_glcm_complex['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_complex['correlation'].append(glcm_eq.correlation())
+    dict_glcm_complex['variance'].append(glcm_eq.variance())
+    dict_glcm_complex['entropy'].append(glcm_eq.entropy())
+    dict_glcm_complex['pathology'].append(1)
+
+    matrix_gl_gen_duplex = glcm.glcm_gen_duplex()
+    probs = count_probabilities_arr(matrix_gl_gen_duplex)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_gen_duplex['name'].append('patho_' + name)
+    dict_glcm_gen_duplex['energy'].append(glcm_eq.energy())
+    dict_glcm_gen_duplex['contrast'].append(glcm_eq.contrast())
+    dict_glcm_gen_duplex['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_gen_duplex['correlation'].append(glcm_eq.correlation())
+    dict_glcm_gen_duplex['variance'].append(glcm_eq.variance())
+    dict_glcm_gen_duplex['entropy'].append(glcm_eq.entropy())
+    dict_glcm_gen_duplex['pathology'].append(1)
+
+    matrix_gl_gen_duplex_complex = glcm.glcm_complex_duplex()
+    probs = count_probabilities_arr(matrix_gl_gen_duplex_complex)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_complex_duplex['name'].append('patho_' + name)
+    dict_glcm_complex_duplex['energy'].append(glcm_eq.energy())
+    dict_glcm_complex_duplex['contrast'].append(glcm_eq.contrast())
+    dict_glcm_complex_duplex['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_complex_duplex['correlation'].append(glcm_eq.correlation())
+    dict_glcm_complex_duplex['variance'].append(glcm_eq.variance())
+    dict_glcm_complex_duplex['entropy'].append(glcm_eq.entropy())
+    dict_glcm_complex_duplex['pathology'].append(1)
+
+    print("Pathology image " + name + " done.")
+    print(str(len(pathology_img_names) - pathology_img_names.index(name)) + " pathology images left.")
+
+for name, img in zip(norma_imgs_names, norma_imgs):
+    glcm = GLCM(img)
+    matrix_gl_0 = glcm.glcm_0()
+
+    probs = count_probabilities_arr(matrix_gl_0)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_0['name'].append('norma_' + name)
     dict_glcm_0['energy'].append(glcm_eq.energy())
     dict_glcm_0['contrast'].append(glcm_eq.contrast())
     dict_glcm_0['homogenity'].append(glcm_eq.homogenity())
@@ -154,7 +311,101 @@ for name, img in zip(norma_imgs_names, norma_imgs):
     dict_glcm_0['entropy'].append(glcm_eq.entropy())
     dict_glcm_0['pathology'].append(0)
 
+    matrix_gl_45 = glcm.glcm_45()
+    probs = count_probabilities_arr(matrix_gl_45)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_45['name'].append('norma_' + name)
+    dict_glcm_45['energy'].append(glcm_eq.energy())
+    dict_glcm_45['contrast'].append(glcm_eq.contrast())
+    dict_glcm_45['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_45['correlation'].append(glcm_eq.correlation())
+    dict_glcm_45['variance'].append(glcm_eq.variance())
+    dict_glcm_45['entropy'].append(glcm_eq.entropy())
+    dict_glcm_45['pathology'].append(0)
+
+    matrix_gl_90 = glcm.glcm_90()
+    probs = count_probabilities_arr(matrix_gl_90)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_90['name'].append('norma_' + name)
+    dict_glcm_90['energy'].append(glcm_eq.energy())
+    dict_glcm_90['contrast'].append(glcm_eq.contrast())
+    dict_glcm_90['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_90['correlation'].append(glcm_eq.correlation())
+    dict_glcm_90['variance'].append(glcm_eq.variance())
+    dict_glcm_90['entropy'].append(glcm_eq.entropy())
+    dict_glcm_90['pathology'].append(0)
+
+    matrix_gl_135 = glcm.glcm_135()
+    probs = count_probabilities_arr(matrix_gl_135)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_135['name'].append('norma_' + name)
+    dict_glcm_135['energy'].append(glcm_eq.energy())
+    dict_glcm_135['contrast'].append(glcm_eq.contrast())
+    dict_glcm_135['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_135['correlation'].append(glcm_eq.correlation())
+    dict_glcm_135['variance'].append(glcm_eq.variance())
+    dict_glcm_135['entropy'].append(glcm_eq.entropy())
+    dict_glcm_135['pathology'].append(0)
+
+    matrix_gl_complex = glcm.glcm_complex()
+    probs = count_probabilities_arr(matrix_gl_complex)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_complex['name'].append('norma_' + name)
+    dict_glcm_complex['energy'].append(glcm_eq.energy())
+    dict_glcm_complex['contrast'].append(glcm_eq.contrast())
+    dict_glcm_complex['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_complex['correlation'].append(glcm_eq.correlation())
+    dict_glcm_complex['variance'].append(glcm_eq.variance())
+    dict_glcm_complex['entropy'].append(glcm_eq.entropy())
+    dict_glcm_complex['pathology'].append(0)
+
+    matrix_gl_gen_duplex = glcm.glcm_gen_duplex()
+    probs = count_probabilities_arr(matrix_gl_gen_duplex)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_gen_duplex['name'].append('norma_' + name)
+    dict_glcm_gen_duplex['energy'].append(glcm_eq.energy())
+    dict_glcm_gen_duplex['contrast'].append(glcm_eq.contrast())
+    dict_glcm_gen_duplex['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_gen_duplex['correlation'].append(glcm_eq.correlation())
+    dict_glcm_gen_duplex['variance'].append(glcm_eq.variance())
+    dict_glcm_gen_duplex['entropy'].append(glcm_eq.entropy())
+    dict_glcm_gen_duplex['pathology'].append(0)
+
+    matrix_gl_gen_duplex_complex = glcm.glcm_complex_duplex()
+    probs = count_probabilities_arr(matrix_gl_gen_duplex_complex)
+    glcm_eq = GLCMEquations(probs)
+    dict_glcm_complex_duplex['name'].append('norma_' + name)
+    dict_glcm_complex_duplex['energy'].append(glcm_eq.energy())
+    dict_glcm_complex_duplex['contrast'].append(glcm_eq.contrast())
+    dict_glcm_complex_duplex['homogenity'].append(glcm_eq.homogenity())
+    dict_glcm_complex_duplex['correlation'].append(glcm_eq.correlation())
+    dict_glcm_complex_duplex['variance'].append(glcm_eq.variance())
+    dict_glcm_complex_duplex['entropy'].append(glcm_eq.entropy())
+    dict_glcm_complex_duplex['pathology'].append(0)
+    print("Norma image " + name + " done.")
+    print(str(len(norma_imgs_names) - norma_imgs_names.index(name)) + " norma images left.")
+
 df_glcm_0 = pd.DataFrame(dict_glcm_0)
+df_glcm_0.to_csv('glcm_0.csv', sep='\t', encoding='utf-8', index=False)
+
+df_glcm_45 = pd.DataFrame(dict_glcm_45)
+df_glcm_45.to_csv('glcm_45.csv', sep='\t', encoding='utf-8', index=False)
+
+df_glcm_90 = pd.DataFrame(dict_glcm_90)
+df_glcm_90.to_csv('glcm_90.csv', sep='\t', encoding='utf-8', index=False)
+
+df_glcm_135 = pd.DataFrame(dict_glcm_135)
+df_glcm_135.to_csv('glcm_135.csv', sep='\t', encoding='utf-8', index=False)
+
+df_glcm_complex = pd.DataFrame(dict_glcm_complex)
+df_glcm_complex.to_csv('glcm_complex.csv', sep='\t', encoding='utf-8', index=False)
+
+df_glcm_duplex = pd.DataFrame(dict_glcm_gen_duplex)
+df_glcm_duplex.to_csv('glcm_duplex.csv', sep='\t', encoding='utf-8', index=False)
+
+df_glcm_complex_duplex = pd.DataFrame(dict_glcm_complex_duplex)
+df_glcm_complex_duplex.to_csv('glcm_complex_duplex.csv', sep='\t', encoding='utf-8', index=False)
+
 
 
 print(df_glcm_0)
@@ -162,10 +413,4 @@ print(df_glcm_0)
 
 # p = count_probabilities_arr(g)
 # #
-# gleq = GLCMEquations(p)
-# print("Energy: ", gleq.energy())
-# print("Contrast: ", gleq.contrast())
-# print("Homogenity: ", gleq.homogenity())
-# print("Correlation: ", gleq.correlation())
-# print("Variance:", gleq.variance())
-# print("Entropy: ", gleq.entropy())
+
