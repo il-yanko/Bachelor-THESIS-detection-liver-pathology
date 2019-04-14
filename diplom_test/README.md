244c4107b9060a20da5debc321f3e0fe3b7a7727
ikachko <ilyakachko@gmail.com>
2019-04-14 06:52:20 -0700

    Deleted old directories

--- diplom_test/README.md
+++
@@ -1,1 +0,0 @@
-# algorithm detects liver pathology
--- diplom_test/src/visualize.py
+++
@@ -1,6 +0,0 @@
-import seaborn as sns
-import pandas as pd
-
-df = pd.read_csv('./diplom_test/glcm_0.csv', sep='\t')
-
-sns.pairplot(df, hue='pathology')
