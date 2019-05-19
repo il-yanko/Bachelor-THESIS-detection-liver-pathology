16bb906f8b9fe61f848ae8ebbef217ab874c4b44
CrackedStone <iljankovy@gmail.com>
2019-05-19 19:59:31 +0300

    fixed mistakes in XGBoost classification and corrected datasets

--- diplom_test/classification.py
+++ diplom_test/classification.py
@@ -21,58 +21,74 @@ chosen = ["original_glrlm_RunEntropy",
           "diagnostics_Image-original_Mean"
           ]
           #,"diagnosis_code"]
-chosen1 = ["original_glcm_Imc1",
-          "original_glcm_ClusterShade",
+chosen1 = [#"original_glcm_Imc1",
+          #"original_glcm_ClusterShade",
           "original_glrlm_LongRunHighGrayLevelEmphasis",
           "original_glrlm_RunLengthNonUniformity"
           ]
-
-#data = pd.read_csv(path, ";")
+chosen2 = [
+            'original_glrlm_RunEntropy',
+            'original_glrlm_GrayLevelNonUniformity',
+            'original_glrlm_GrayLevelVariance',
+            'original_glrlm_LongRunHighGrayLevelEmphasis',
+            'original_glrlm_LongRunLowGrayLevelEmphasis',
+            'original_glrlm_RunLengthNonUniformity',
+          ]
+data = pd.read_csv(path, ";")
 # visualize SEABORN
-'''
+
 red_blue = ["#ff0000", "#1240ab"]
 sns.pairplot(
     data,
     vars=[
-        #'original_glrlm_RunEntropy',
-        #'original_glrlm_GrayLevelNonUniformity',
-        #'original_glrlm_GrayLevelVariance',
-        'original_glrlm_LongRunHighGrayLevelEmphasis',
-        #'original_glrlm_LongRunLowGrayLevelEmphasis',
-        'original_glrlm_RunLengthNonUniformity',
+          "original_glrlm_LongRunHighGrayLevelEmphasis",
+          "original_glrlm_RunLengthNonUniformity"
     ],
     #hue='data_source', 'isnorm'
-    hue='data_source',
+    hue='isnorm',
     aspect=0.3,
-    palette="Paired",
+    palette=red_blue,
     #kind="skatter"
     #markers="."
 )
 plt.show()
 plt.tight_layout()
-'''
 
+
+'''
 # download train and test data
 test_path = "data/result/test.csv"
 test = pd.read_csv(test_path, ";")
 train_path = "data/result/train.csv"
 train = pd.read_csv(train_path, ";")
-#X = train[chosen1]
-X_train = train.iloc[:, 1:train.shape[1] - 7]
-y_train = train["isnorm"]
-X_test = test.iloc[:, 1:train.shape[1] - 7]
-y_test = test["isnorm"]
 
-# Make Logistic Regression (Logit) and test it
+#X_train = train.iloc[:, 1:train.shape[1] - 7]
+X_train = train[chosen1]
+y_train = train["isnorm"]
+#X_test = test.iloc  [:, 1:train.shape[1] - 7]
+X_test = test[chosen1]
+y_test = test["isnorm"]
 '''
-clf = linear_model.LogisticRegression(C=1e5, solver='lbfgs')
+
+
+
+
+# Make Multinomial Logistic Regression (Logit) and test it
+'''
+print("\nMultinomial Logistic Regression:\n===================")
+clf = linear_model.LogisticRegression(max_iter=10000, C=1e5, solver='lbfgs',multi_class='multinomial')
 clf.fit(X_train, y_train)
 # Test the classifier
-clf.predict_proba(X_test)
-print(clf.score(X_train, y_train))
+y_pred = clf.predict(X_test)
+print("Accuracy:%.2f%%" % float(accuracy_score(y_test, y_pred))*100)
+print("Prediction:\n",y_pred)
+print("Real test:\n",y_test.to_numpy())
 '''
 
+
 # XGBoost
+'''
+print("\nXGBoost Classification:\n===================")
 # fit model on all training data
 model = XGBClassifier()
 model.fit(X_train, y_train)
@@ -81,11 +97,15 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 predictions = [round(value) for value in y_pred]
 accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+print("Accuracy: %.4f%%%%" % (accuracy * 100.0))
 
 # Fit model using each importance as a threshold
 thresholds = np.sort(model.feature_importances_)
-print("%.2f%", thresholds)
+#print("%.2f%", thresholds)
+'''
+
+# cycle
+'''
 for thresh in thresholds:
     # select features using threshold
     selection = SelectFromModel(model, threshold=thresh, prefit=True)
@@ -99,3 +119,19 @@ for thresh in thresholds:
     predictions = [round(value) for value in y_pred]
     accuracy = accuracy_score(y_test, predictions)
     print("Thresh=%.3f, n=%d, Accuracy: %.2f%%" % (thresh, select_X_train.shape[1], accuracy*100.0))
+'''
+# hand-made threshold
+'''
+# select features using threshold
+selection = SelectFromModel(model, threshold=0.06, prefit=True)
+select_X_train = selection.transform(X_train)
+# train model
+selection_model = XGBClassifier()
+selection_model.fit(select_X_train, y_train)
+# eval model
+select_X_test = selection.transform(X_test)
+y_pred = selection_model.predict(select_X_test)
+predictions = [round(value) for value in y_pred]
+accuracy = accuracy_score(y_test, predictions)
+print("Thresh=%.3f, n=%d, Accuracy: %.2f%%" % (0.06, select_X_train.shape[1], accuracy*100.0))
+'''
